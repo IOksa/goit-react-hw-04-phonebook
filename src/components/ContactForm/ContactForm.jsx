7aecc63fc0dd48d700f5205f4cfa8455dbe473da
IOksa <117656767+IOksa@users.noreply.github.com>
2023-06-24 18:40:28 +0300

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,65 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import css from './ContactForm.module.css';
 
-class ContactForm extends Component{
-    state = {
-        name: '',
-        number: '',
-    };
+const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
 
-
-    handleChange = ({ target: { name, value } }) => {
-        this.setState({ [name]: value });
-
-    };
+    const handleChange = evt => {
+        const { name, value } = evt.target;
     
-    handleSubmit = e => {
+        switch (name) {
+          case 'name':
+            setName(value);
+            break;
+    
+          case 'number':
+            setNumber(value);
+            break;
+    
+          default:
+            console.warn(`Тип поля name - ${name} не обрабатывается`);
+        }
+      };
+
+
+    
+    const handleSubmit = e => {
         e.preventDefault();
        
-        this.props.onSubmit(this.state);
+        //this.props.onSubmit(this.state); ДОДЕЛАТЬ
     
-        this.setState({ name: '', number: '' });
+        setName('');
+        setNumber('');
     };
  
 
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit} className={css.phonebook__formContact}>
-                <label className={css.phonebook__formContactLabel}>Name</label>
-                <input
-                    type="name"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    className={css.phonebook__formContactInput}
-                    />
-                
-                <label className={css.phonebook__formContactLabel}>Number</label>
-                <input
-                    type="tel"
-                    name="number"
-                    pattern="^[0-9]+$"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    value={this.state.number}
-                    onChange={this.handleChange}
-                    className={css.phonebook__formContactInput}
-                    />
-               
-                <button type="submit" className={css.form__button}>
-                Add contact
-                </button>
-            </form>
-        
-        
-        );
+   
+    return(
+        <form onSubmit={handleSubmit} className={css.phonebook__formContact}>
+            <label className={css.phonebook__formContactLabel}>Name</label>
+            <input
+                type="name"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={name}
+                onChange={handleChange}
+                className={css.phonebook__formContactInput}
+                />
+            
+            <label className={css.phonebook__formContactLabel}>Number</label>
+            <input
+                type="tel"
+                name="number"
+                pattern="^[0-9]+$"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                value={number}
+                onChange={handleChange}
+                className={css.phonebook__formContactInput}
+                />
+            
+            <button type="submit" className={css.form__button}>
+            Add contact
+            </button>
+        </form>
+    
+    
+    );
 
 
-    }  
+    
 }
 
 export default ContactForm;
